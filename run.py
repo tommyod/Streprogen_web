c0c560aac8980a2195fccc3018a0d1e0227489e3
Werner Sævland <werner.saevland@gmail.com>
2017-03-17 10:19:04 +0100

    First real commit

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask_debugtoolbar import DebugToolbarExtension
 
 if __name__ == '__main__':
-    app.debug = True
-    toolbar = DebugToolbarExtension(app)
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    app.run(port=8100)
+    app.run() #port = int(os.environ.get('PORT', 5000)
